5ceb093b4833b794edd87c266e2d59f12478469e
R0mAN <aswinbabu06@gmail.com>
2020-04-06 11:11:35 +0200

    Updated GUI

--- Chatbox_Client.py
+++ Chatbox_Client.py
@@ -3,14 +3,17 @@ from tkinter import *
 from tkinter.ttk import *
 from threading import *
 
-flag = True #To terminate thread
+#To terminate thread
+flag = True
 
-def reader():#Send msgs and update Treeview
+#Send msgs and update message list
+def reader():
     msg = box.get()
-    tree.insert('', 'end', text=f'|->: {msg}')
+    msg_lst.insert(END, f'|->: {msg}')
     c.send(bytes(msg, 'utf-8'))
 
-def end_conn(var):#Close connection and disable buttons
+#Close connection and disable buttons
+def end_conn(var):
     global flag
     if var == 0:
         c.send(bytes(':END IT:', 'utf-8'))
@@ -19,31 +22,40 @@ def end_conn(var):#Close connection and disable buttons
     read.config(state=DISABLED)
     end.config(state=DISABLED)
 
-def msg_updater():#To check and read msgs from client
+#To check and read msgs from client
+def msg_updater():
     global flag
     while flag:
         msg_read = c.recv(1024).decode()
         if msg_read:
-            tree.insert('', 'end', text=f'{client_name}: {msg_read}')
+            msg_lst.insert(END, f'{client_name}: {msg_read}')
         if msg_read == ":END IT:":
             end_conn(1)
 
 
-#GUI
+#Thread for message update
 t1 = Thread(target=msg_updater)
+
+#GUI
 window = Tk()
 window.title('Chatbox')
-heading = Label(window,text='Chatbox',width=35).grid(row=1,column=0)
-box = Entry(window)
+heading = Label(window,text='Chatbox:: |->',width=35).grid(row=1,column=0)
+box = Entry(window,width=40)
 box.grid(row=2,column=0)
 read = Button(window,text='Send',command=reader)
 read.grid(row=2,column=1)
-tree = Treeview(window)
-tree.grid(row=0,column=0)
+main_frame = LabelFrame(window)
+main_frame.grid(row=0,column=0)
+msg_scroll = Scrollbar(main_frame,orient="horizontal")
+msg_scroll.pack(side=BOTTOM,fill=X)
+msg_lst = Listbox(main_frame,width=60)
+msg_lst.pack()
+msg_lst.config(xscrollcommand=msg_scroll.set)
+msg_scroll.config(command=msg_lst.xview)
 
 #Socket setup
 c = socket.socket()
-#c.bind(('localhost',22222))
+c.bind(('localhost',22222))
 print("Socket with port:22222 Initiated")
 c.connect(("localhost",11111))
 
@@ -53,7 +65,7 @@ client_name = c.recv(1024).decode()
 
 print(f'Connected with {client_name} at 11111')
 Conn_info = Label(window,text=f'Logged in as {name}\n\nConnected to {client_name}').grid(row=0,column=1)
-c.send(bytes('Connected to the Server','utf-8'))
+c.send(bytes('Connected','utf-8'))
 
 end = Button(window,text='End Connection',command=lambda : end_conn(0))
 end.grid(row=1,column=1)
